6620fc686e2dc9e1ad31184dd9dc9d953236843b
jaimeXziim <167575506+jaimeXziim@users.noreply.github.com>
2025-03-18 23:37:35 +0000

    Exibição das informações da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,28 @@
 
 int main() {
     // Declaração de variaveis para a primeira carta
+    char estado1[20] = "Ceará";
+    char codigo1[5] = "C01";
+    char nomeCidade1[50] = "Fortaleza";
+    int populacao1 = 2687000;
+    float area1 = 312.35;
+    float pib1 = 67.45;
+    int pontosTuristicos1 = 15;
+
+    // Declaração de variaveis para a segunda carta
+    char estado2[20] = "Paraná";
+    char codigo2[5] = "P01";
+    char nomedaCidade2[50] = "Coritiba";
+    int popolucao2 = 1963726;
+    float area2 = 435.04;
+    float pib2 = 101.32;
+    int pontosTuristicos2 = 19;
+
+    // Exibição das informações da primeira carta
+    printf("\nCarta 1:\n");
+    printf("Estado: %s\n, estado1");
+    printf("Código: %s\n, codigo1");
+    Printf("Cidade: %s\n, nomeCidade1");
 
 
 
